9ff3bbbd29523510c678315b17eeab6829f33fba
Serdar YILLAR <serdar.yillar@pordiva.com>
2015-06-03 15:07:48 +0300

    firstcommit

---
+++ podtest/podtest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  podtest
+//
+//  Created by Serdar Yıllar on 03/06/15.
+//  Copyright (c) 2015 Pordiva. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
