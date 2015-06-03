6c9d1900055f0b1775a3fdf0376671b841b70234
Serdar YILLAR <serdar.yillar@pordiva.com>
2015-06-03 15:23:37 +0300

    no message

---
+++ podtest/podtest/PodLib/TestController.m
@@ -0,0 +1,37 @@
+//
+//  TestController.m
+//  podtest
+//
+//  Created by Serdar Yıllar on 03/06/15.
+//  Copyright (c) 2015 Pordiva. All rights reserved.
+//
+
+#import "TestController.h"
+
+@interface TestController ()
+
+@end
+
+@implementation TestController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
